83368ce95f2395f523d6f8f075d044aec08c5c5b
Alex Castrelo <neoares23@gmail.com>
2019-04-14 18:23:45 +0200

    Add libraries and stuff

--- web/dashboard.js
+++ web/dashboard.js
@@ -9,22 +9,90 @@ function showError(el, error){
 
 var embedOpt = {"mode": "vega-lite"};
 
-var map_file = "graphs/map.json";
-const map_el = document.getElementById('map');
-vegaEmbed("#map", map_file, embedOpt)
-  .catch(error => showError(map_el, error));
+function draw(selector, data, options=embedOpt) {
+    var el = document.querySelector(selector);
+    vegaEmbed(selector, data, options)
+        .catch(error => showError(el, error));
 
-var chart1_file = "graphs/graph_quant_fam.json";
-const chart1_el = document.getElementById('chart1');
-vegaEmbed("#chart1", chart1_file, embedOpt)
-  .catch(error => showError(chart1_el, error));
+}
 
-var chart2_file = "graphs/graph_quant_fam.json";
-const chart2_el = document.getElementById('chart2');
-vegaEmbed("#chart2", chart2_file, embedOpt)
-  .catch(error => showError(chart2_el, error));
+var graphs = {'map': undefined, 'quant_fam': undefined, 'balls': undefined, 'quant_com': undefined, 'ben_com': undefined}
 
-var chart3_file = "graphs/graph_quant_fam.json";
-const chart3_el = document.getElementById('chart3');
-vegaEmbed("#chart3", chart3_file, embedOpt)
-  .catch(error => showError(chart3_el, error));
+// MAP
+fetch(window.location.origin + '/graphs/map.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        myJson.data.url = 'graphs/' + myJson.data.url
+        graphs.map = myJson
+        draw('#map', myJson)
+    });
+
+// QUANT-FAM
+fetch(window.location.origin + '/graphs/graph_quant_fam.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        graphs.quant_fam = myJson
+        draw('#quant-fam', myJson)
+    });
+
+// BALLS
+fetch(window.location.origin + '/graphs/balls_bqy.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        graphs.balls = myJson
+        draw('#ball', myJson)
+    });
+
+// QUANTITAT / COMARCA
+fetch(window.location.origin + '/graphs/graph_quant_com.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        graphs.quant_com = myJson
+        draw('#quant-com', myJson)
+    });
+
+// BENEFICI / COMARCA
+fetch(window.location.origin + '/graphs/graph_benef_comarca.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        graphs.ben_com = myJson
+        draw('#ben-com', myJson)
+    });
+
+
+var which = 1
+function swap_level(event) {
+    l1 = document.querySelectorAll('#main-grid .level-1');
+    l2 = document.querySelectorAll('#main-grid .level-2');
+    label = document.querySelector('#change-view span.text ')
+
+    if (which == 2) {
+        which = 1
+        Array.from(l1).map(el => {el.style.display = ''})
+        Array.from(l2).map(el => {el.style.display = 'none'})
+        label.innerHTML = 'View details'
+    } else {
+        which = 2
+        Array.from(l1).map(el => {el.style.display = 'none'})
+        Array.from(l2).map(el => {el.style.display = ''})
+        label.innerHTML = 'View generic'
+    }
+}
+
+l2 = document.querySelectorAll('#main-grid .level-2');
+Array.from(l2).map(el => {el.style.display = 'none'})
+
+document.querySelector('#content nav .more-info').addEventListener('click', event => {
+    console.log("clicked")
+    swap_level(event)
+})
